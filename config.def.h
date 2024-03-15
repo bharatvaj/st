c84c37dc5e8e3c8894f9ba8ea8f661543013ce9e
Bharatvaj Hemanth <bharatvaj@yahoo.com>
2024-03-15 15:22:43 +0530

    Apply the following patches, 	* st-clipboard 	* st-scrollback-ringbuffer 	* st-scrollback-mouse 	* st-gruvbox-material 	* st-invert

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -96,24 +96,25 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#ea6962", /* red     */
+	[2] = "#a9b665", /* green   */
+	[3] = "#d8a657", /* yellow  */
+	[4] = "#7daea3", /* blue    */
+	[5] = "#d3869b", /* magenta */
+	[6] = "#89b482", /* cyan    */
+	[7] = "#d4be98", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "#928374", /* black   */
+	[9]  = "#ef938e", /* red     */
+	[10] = "#bbc585", /* green   */
+	[11] = "#e1bb7e", /* yellow  */
+	[12] = "#9dc2ba", /* blue    */
+	[13] = "#e1acbb", /* magenta */
+	[14] = "#a7c7a2", /* cyan    */
+	[15] = "#e2d3ba", /* white   */
+
 
 	[255] = 0,
 
@@ -121,7 +122,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#222222", /* default background colour */
 };
 
 
@@ -176,7 +177,9 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ 0,                    Button4, kscrollup,      {.i = 2} },
+	{ 0,                    Button5, kscrolldown,    {.i = 2} },
+	{ XK_ANY_MOD,           Button2, clippaste,      {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
@@ -199,8 +202,11 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_X,           invert,         { }       },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
